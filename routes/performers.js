3612abe4d83261a9197e1bb763cfd39879f40677
msiller455 <msiller455@gmail.com>
2022-10-06 16:50:16 -0700

    finishes mongoose movies

--- routes/performers.js
+++ routes/performers.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const performersCtrl = require('../controllers/performers');
+const isLoggedIn = require('../config/auth')
 
 // This router is mounted to a "starts with" path of '/'
 
 // GET /performers/new
-router.get('/performers/new', performersCtrl.new);
+router.get('/performers/new', isLoggedIn, performersCtrl.new);
 // POST /performers
-router.post('/performers', performersCtrl.create);
+router.post('/performers', isLoggedIn, performersCtrl.create);
 router.post('/movies/:id/performers', performersCtrl.addToCast)
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 // You Do - Require the yet to be created reviews controller 
 const reviewsCtrl = require('../controllers/reviews')
+const isLoggedIn = require('../config/auth')
 
 // You Do - Define the Route below
-router.post('/movies/:id/reviews', reviewsCtrl.create)
-router.delete('/reviews/:id', reviewsCtrl.delete)
+router.post('/movies/:id/reviews', isLoggedIn,  reviewsCtrl.create)
+router.delete('/reviews/:id', isLoggedIn,  reviewsCtrl.delete)
 
 module.exports = router
